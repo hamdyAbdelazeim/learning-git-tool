1fe2c1b0637671f1822af9b56ec6b01d58dd283a
Hamdy Abdelazeim <hamdyabdelazeim@gmail.com>
2023-01-21 20:21:48 +0200

    adding express

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
-let names = 'hammy'
-console.log(names)
-let newVariable = 2
-var arr = [1, 2, 3, 4]
-let newEdit = 12
-let ourCode = 12
+let const express = require('express')
+const app = express()
+const port = 3000
 
-// hello code
-console.log(ourCode)
-ourCode = 12
-console.log(ourCode)
-
-// let and const
-let ourName = 'Hammy'
-console.log(ourName)
-// adding males
-let addingMales = 'Hammy'
-console.log(addingMales)
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
