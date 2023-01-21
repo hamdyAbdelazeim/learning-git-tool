5d7070943a17ded29d33c8d51bb43de690eae2a0
Hamdy Abdelazeim <hamdyabdelazeim@gmail.com>
2023-01-21 11:20:20 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let names = 'hammy'
+console.log(names)
+let newVariable = 2
+var arr = [1, 2, 3, 4]
+let newEdit = 12
+let ourCode = 12
+
+// hello code
+console.log(ourCode)
+ourCode = 12
+console.log(ourCode)
+
+// let and const
+let ourName = 'Hammy'
+console.log(ourName)
